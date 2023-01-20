068115e1531a1f3b273b73fff2dbbf02fbf05438
Eric Tsai <hiphop200199@gmail.com>
2023-01-20 11:45:13 +0800

    上傳資料

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  const bgm = new Audio("./assets/piano-moment.mp3");
+  function music() {
+    bgm.play();
+  }
+</script>
+
+<article>
+  <button on:click={music}>&#x266B;</button>
+  <h1>Birthday wishes</h1>
+  <input type="text" placeholder="write your wishes..." />
+  <button type="button">ok</button>
+</article>
