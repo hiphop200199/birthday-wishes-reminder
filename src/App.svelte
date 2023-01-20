baebf84d576b3df27c4517e92b603a22310741c2
Eric Tsai <hiphop200199@gmail.com>
2023-01-20 12:14:50 +0800

    file upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script>
-  const bgm = new Audio("./assets/piano-moment.mp3");
-  function music() {
-    bgm.play();
-  }
+  import { musicData } from "./musicData";
 </script>
 
 <article>
-  <button on:click={music}>&#x266B;</button>
+  <audio src="">&#x266B;</audio>
   <h1>Birthday wishes</h1>
   <input type="text" placeholder="write your wishes..." />
   <button type="button">ok</button>
